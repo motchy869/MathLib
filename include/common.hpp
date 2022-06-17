813069699f6ab5344242c2902e33265695f0052f
motchy <motchy869@gmail.com>
2022-06-17 03:36:47 +0000

    fix: outdated version macro constants

--- include/common.hpp
+++ include/common.hpp
@@ -4,9 +4,9 @@
  */
 #pragma once
 #define MATH_LIB_VER_MAJOR 0
-#define MATH_LIB_VER_MINOR 4
-#define MATH_LIB_VER_PATCH 2
-#define MATH_LIB_VER_ALPHA 1 // 0 if release version, 1 if alpha version
+#define MATH_LIB_VER_MINOR 7
+#define MATH_LIB_VER_PATCH 0
+#define MATH_LIB_VER_ALPHA 0 // 0 if release version, 1 if alpha version
 #define MATH_LIB_ENABLE_CANARY_MODE false
 #define MATH_LIB_INLINE_AGGRESSIVELY true
 
